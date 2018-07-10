598abf8cf56aac13616f9b7413be2ad324969d07
anff <anff18792647864@163.com>
2018-07-10 15:36:06 +0800

    第一次提交
    第一次提交

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="hello">
+    <my-header> </my-header>
+
+    <text-input></text-input>
+
+    <!--<h1>{{ msg }}</h1>-->
+    <!--<h2>Ecosystem</h2>-->
+    <!--<ul>-->
+      <!--<li>-->
+        <!--<a href="https://github.com/vuejs/awesome-vue" target="_blank" > awesome-vue </a>-->
+      <!--</li>-->
+    <!--</ul>-->
+
+    <my-footer> </my-footer>
+  </div>
+</template>
+
+<script>
+
+import MyHeader from './common/myHeader'
+import MyFooter from './common/myFooter'
+import TextInput from './pages/textInput'
+
+export default {
+
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  components: {
+    MyHeader,
+    MyFooter,
+    TextInput
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/components/common/myFooter.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="is-center">
+      这他妈才是底部！
+    </div>
+</template>
+
+<script>
+ export default {
+   name: 'my-footer'
+ }
+</script>
+
+<style scoped>
+  div {
+    position: fixed;
+    bottom: 0;
+    color: #67a537;
+  }
+
+</style>
